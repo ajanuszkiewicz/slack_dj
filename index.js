d310b3177f82cb5faad5a2b4d345a371b51053ea
ajanuszkiewicz <adamj@Adams-MacBook-Air.local>
2014-12-19 15:52:43 -0500

    Whooo

--- index.js
+++ index.js
@@ -50,7 +50,6 @@ app.post('/', function(req, res){
 
 //playSongAlbum ('smells like teen spirit - nirvana');
 //playSongAlbum ('bit by bit by mother mother');
-PostCode();
 
 function playSongAlbum (request) {
 
@@ -115,40 +114,3 @@ io.on('connection', function(socket){
     console.log('message: ' + msg); 
   });
 });
-
-
-// function PostCode(codestring) {
-//   // Build the post string from an object
-//   var post_data = querystring.stringify({
-//       'compilation_level' : 'ADVANCED_OPTIMIZATIONS',
-//       'output_format': 'json',
-//       'output_info': 'compiled_code',
-//         'warning_level' : 'QUIET',
-//         'js_code' : codestring
-//   });
-
-//   // An object of options to indicate where to post to
-//   var post_options = {
-//       host: 'https://hooks.slack.com/services/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',
-//       port: '80',
-//       path: '/',
-//       method: 'POST',
-//       headers: {
-//           'Content-Type': 'application/x-www-form-urlencoded',
-//           'Content-Length': post_data.length
-//       }
-//   };
-
-//   // Set up the request
-//   var post_req = http.request(post_options, function(res) {
-//       res.setEncoding('utf8');
-//       res.on('data', function (chunk) {
-//           console.log('Response: ' + chunk);
-//       });
-//   });
-
-//   // post the data
-//   post_req.write(post_data);
-//   post_req.end();
-
-// }
