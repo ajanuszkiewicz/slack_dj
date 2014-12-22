f426e87ac6a7227da2cdcd43264f907ef610aff4
ajanuszkiewicz <adamj@adams-air.lan>
2014-12-21 22:53:52 -0500

    Added random song

--- index.js
+++ index.js
@@ -11,10 +11,13 @@ var rdio = new Rdio(["ppebg64vtgcxat45rhrven46", "9YT2zjhE63"]);
 var server =http.createServer(app);
 var io = _io.listen(server);
 
-var	artistkey = "";
-var	songkey = "";
-var	check = "";
-
+var aftertext;
+var beforetext;
+var	artistkey;
+var artistname;
+var	songkey;
+var songname;
+var	check;
 var skipcounter = 0;
 
 server.listen(process.env.PORT || 3000);
@@ -40,71 +43,77 @@ app.post('/', function(req, res){
 
 
 fullstr = getQueryVariable(data,'text');
-string = fullstr.replace('dj:+','');
+fullstr = fullstr.replace(/\+/g," ");
 
 console.log(fullstr);
-io.emit('chat message', string);
+io.emit('chat message', fullstr);
 
-if (string.search("play") != -1){
+if (fullstr.search("dj: play") != -1){
+	string = fullstr.replace('dj: play ','');
 	playSongAlbum(string);
-} else if (string.search("skip") != -1){
+	data = "";
+} else if (fullstr.search("dj: skip") != -1){
 	skipSong();
+} else if (fullstr.search("dj: random") != -1){
+	string = fullstr.replace('dj: random ','');
+	randomSong(string);
+	data = "";
 } else {
 	data = "";
 }
 
-function skipSong (){
-	skipcounter ++;
-	msg = "skip";
-	if (skipcounter == 3){
-		io.emit('control message', msg);
-		res.send({ "text": "Strike 3! The song has been vetoed by your peers." });
-		skipcounter = 0;
-	} else{
-		string = "Stike " + skipcounter + "!";
-		res.send({ "text": string });
-	}
-
-	data = "";
-}
-
-
 //playSongAlbum ('smells like teen spirit - nirvana');
 //playSongAlbum ('bit by bit by mother mother');
 //playSongAlbum ('lola by the kinks');
+//randomSong("bonobo");
 
 function playSongAlbum (request) {
 
 	console.log(request);
 
-	genstring = request.replace('play+','');
-	genstring = genstring.replace(/\+/g," ");
-	aftertext = genstring.substr(genstring.lastIndexOf("by") + 3);
-	beforetext = genstring.substr(0, genstring.lastIndexOf('by'));
+	aftertext = request.substr(request.lastIndexOf("by") + 3);
+	beforetext = request.substr(0, request.lastIndexOf('by'));
 
 	console.log(aftertext);
 	console.log(beforetext);
 
-	findArtist();
+	findArtist(aftertext,function(text1, text2){
+		findTrack(beforetext,function(text1, text2, text3){
+			nextStep();
+		});
+	});
 }
 
-function findArtist(){
-	rdio.call('search', {'query': aftertext, 'types': 'artist'}, function (err, data){
-         if (err) {
+function randomSong (request) {
+
+	console.log(request);
+
+	findArtist(request,function(text1, text2){
+		artistTracks(artistkey,function(text1, text2, text3){
+			nextStep();
+		});
+	});
+}
+
+function findArtist(input, callback){
+	rdio.call('search', {'query': input, 'types': 'artist'}, function (err, data){
+        if (err) {
             return;
+        } else if (data.result.artist_count == '0'){
+        	res.send({ "text": "Sorry, I couldn't find that artist." });
+        	return;
         }
 
 		artistkey = data.result.results[0].key;
 		artistname = data.result.results[0].name;
 		io.emit('chat message', artistkey);
 
-		findTrack(artistkey);
+		callback(artistkey, artistname);
 	});
-
 }
 
-function findTrack(){
-	rdio.call('search', {'query': beforetext, 'types': 'track', 'count': '50'}, function (err, track){
+function findTrack(input, callback){
+	rdio.call('search', {'query': input, 'types': 'track', 'count': '50'}, function (err, track){
 		 if (err) {
 		    return;
 		}
@@ -136,8 +145,35 @@ function findTrack(){
 
 		io.emit('chat message', songkey);
 		io.emit('chat message', check);
-	
-		nextStep();	
+
+		console.log("Second Check");	
+
+		callback(songkey, songname, check);
+
+	});
+}
+
+function artistTracks(input, callback){
+
+	rdio.call('getTracksForArtist', {'artist': input, 'count': '50'}, function (err, track){
+		 if (err) {
+		    return;
+		}
+
+		var obj = [];
+
+		console.log('random length: ' + track.result.length);
+		var rand = Math.floor((Math.random()* track.result.length) + 1);
+		obj.push(track.result[rand]);
+
+		songkey = obj[0].key;
+		songname = obj[0].name;
+		check = obj[0].artistKey;
+
+		io.emit('chat message', songkey);
+		io.emit('chat message', check);
+
+		callback(songkey, songname, check);
 
 	});
 }
@@ -152,7 +188,6 @@ function nextStep(){
 
 				string = "Added " + songname + " by " + artistname + " to play queue.";
 				res.send({ "text": string });
-
 				io.emit('music message', songkey, beforetext, aftertext);
 				io.emit('chat message', string);
 
@@ -168,8 +203,21 @@ function nextStep(){
 	data = "";
 }
 
+function skipSong (){
+	skipcounter ++;
+	msg = "skip";
+	if (skipcounter == 3){
+		io.emit('control message', msg);
+		res.send({ "text": "Strike 3! The song has been vetoed by your peers." });
+		skipcounter = 0;
+	} else{
+		string = "Stike " + skipcounter + "!";
+		res.send({ "text": string });
+	}
 
-	    
+	data = "";
+}
+   
   	});
 });
 
